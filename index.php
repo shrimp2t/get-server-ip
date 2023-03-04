15548eae4dc77cb596db774031b8ca5ca87e2c73
TruongSa <shrimp2t@gmail.com>
2023-03-04 10:04:17 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name:  My Server IP
+ * Version:      1.14.0
+ * Text Domain:  shrimp2t
+ * Domain Path:  /languages/
+ * Requires PHP: 5.3.6
+ * License:      GPL v2 or later
+ */
+ 
+
+add_action('wp_loaded', function(){
+	if(isset($_GET['ft_ip'])){
+		$res = wp_remote_get('http://ip-api.com/json/');
+		$body = wp_remote_retrieve_body( $res );
+		echo '<pre>'.print_r( $body ) .'</pre>';
+		die();
+	}
+});
+
+
+
